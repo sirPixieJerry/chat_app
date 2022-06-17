e7398e73a8df49e6842e38f4494694e7c8dcd5f2
sirPixieJerry <jerome.bucquet@gmail.com>
2022-06-17 13:28:34 +0200

    readme setup

--- sql/bycrypt.js
+++ sql/bycrypt.js
@@ -1,12 +1,12 @@
 // require bycrypt module to hash passwords
 const bcrypt = require("bcryptjs");
 
-// function to hash passwords ❌
+// function to hash passwords
 module.exports.hashPassword = function (password) {
     return bcrypt.genSalt().then((salt) => bcrypt.hash(password, salt));
 };
 
-// function to match password ❌
+// function to match password
 module.exports.matchPassword = function (password, password_hash, id) {
     return bcrypt
         .compare(password, password_hash)
